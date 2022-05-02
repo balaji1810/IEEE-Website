295d1960cc7e0f20d0333e3d63a517ef276da79d
Balaji S B <balajibavimane@gmail.com>
2022-05-02 20:55:20 +0530

    Added footer

--- index.js
+++ index.js
@@ -1,65 +1,14 @@
-var count = 175;
-
-var path = new Path.Circle({
-    center: new Point(0, 0),
-    radius: 5,
-    fillColor: 'white',
-    strokeColor: 'black'
-});
-
-var symbol = new SymbolDefinition(path);
-
-for (var i = 0; i < count; i++) {
-    var center = Point.random() * view.size;
-    var placed = symbol.place(center);
-    var scale = (i + 1) / count;
-    placed.scale(scale);
-    placed.data.vector = new Point({
-        angle: Math.random() * 360,
-        length : scale * Math.random() / 5
-    });
-}
-
-var vector = new Point({
-    angle: 45,
-    length: 0
-});
-
-var mouseVector = vector.clone();
-
-function onMouseMove(event) {
-    mouseVector = view.center - event.point;
-}
-
-function onFrame(event) {
-    vector = vector + (mouseVector - vector) / 30;
-
-    for (var i = 0; i < count; i++) {
-        var item = project.activeLayer.children[i];
-        var size = item.bounds.size;
-        var length = vector.length / 130 * size.width / 10;
-        item.position += vector.normalize(length) + item.data.vector;
-        keepInView(item);
-    }
-}
-
-function keepInView(item) {
-    var position = item.position;
-    var itemBounds = item.bounds;
-    var bounds = view.bounds;
-    if (itemBounds.left > bounds.width) {
-        position.x = -item.bounds.width/2;
-    }
-
-    if (position.x < -itemBounds.width) {
-        position.x = bounds.width + itemBounds.width;
-    }
-
-    if (itemBounds.top > view.size.height) {
-        position.y = -itemBounds.height;
-    }
-
-    if (position.y < -itemBounds.height) {
-        position.y = bounds.height  + itemBounds.height;
-    }
-}
+// window.onscroll = () => {scrollNavbar()};
+//
+// scrollNavbar = () => {
+//     // Target elements
+//     const navBar = document.getElementById("navbar");
+//
+//   if (document.documentElement.scrollTop > 100) {
+//     navBar.classList.add("light");
+//     navBar.classList.remove("transparent")
+//   } else {
+//     navBar.classList.remove("light");
+//     navBar.classList.add("transparent");
+//   }
+// }
